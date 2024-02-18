bf03b53e45011f018690a27bc46d827df9243f16
anwarsiddiqui018 <siddiqui.anwar018@gmail.com>
2024-02-18 22:57:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+// import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Upload from "./components/Upload";
+
+function Calculator() {
+  return (
+    <div>
+      <h1>Calculator</h1>
+      {/* Add your calculator component here */}
+    </div>
+  );
+}
+
+function Weather() {
+  return (
+    <div>
+      <h1>Weather</h1>
+      {/* Add your weather component here */}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Calculator />
+        </Col>
+        <Col>
+          <Weather />
+        </Col>
+        <Col>
+          <Upload />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
